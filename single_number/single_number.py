3a2782c864cf47c10733b246aaac3b2c64191925
unknown <ccurry20@gmail.com>
2020-06-14 14:10:27 -0400

    Inital Commit

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,8 +4,10 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
-
-    pass
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] == arr[j]:
+                print(arr[i])
 
 
 if __name__ == '__main__':
